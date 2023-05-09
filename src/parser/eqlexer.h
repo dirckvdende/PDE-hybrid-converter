249792e76c1bde72974a53c9fe4a520e0b03f647
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-09 12:35:11 +0200

    Added equation lexer

--- src/parser/eqlexer.h
+++ src/parser/eqlexer.h
@@ -36,9 +36,57 @@ public:
 
 private:
 
+    /**
+     * Get the character at the current position in the input text
+     * @return The character as position pos, or '\0' if reading past the end of
+     * the string
+     */
+    char cur() const;
+
+    /**
+     * Move to the next character in the input string
+     */
+    void next();
+
+    /**
+     * Check if the current position is past the end of the input string
+     * @return A boolean indicating if pos is larger than the txt size
+     */
+    bool atEnd() const;
+
+    /**
+     * Check if a character is whitespace
+     * @return A boolean indicating if a character is considered whitespace
+     */
+    static bool isWhitespace(char c);
+
+    /**
+     * Check if a character is valid for an identifier
+     * @return A boolean indicating if a character is considered valid for an
+     * identifier
+     */
+    static bool isIdentChar(char c);
+
+    /**
+     * Read a number token
+     */
+    void readNum();
+
+    /**
+     * Read an identifier or a derivative
+     */
+    void readIdent();
+
+    /**
+     * Read a special character, such as a brace or a plus sign
+     */
+    void readSpecialChar();
+
     // Input text
     const std::string &txt;
     // Output tokens
     std::vector<EquationToken> tokens;
+    // Current position in the input text
+    size_t pos;
 
 };
