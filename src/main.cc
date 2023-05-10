5359a9ca77c5ea105736251cee620cd70fd965d5
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-10 21:14:16 +0200

    Changed main.cc testing code

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,25 @@
 
 #include "expr/lexer.h"
 #include "expr/parser.h"
+#include "grid/domain.h"
 #include <iostream>
 
-int main() {
-    std::string inp = "5 + 7 * 8";
+void getExpr(const std::string &inp, ExprNode &node) {
     ExprLexer lexer(inp);
     lexer.run();
     ExprParser parser(lexer.getTokens());
     parser.run();
-    std::cout << parser.getTree().str() << std::endl;
+    node = parser.getTree();
+}
+
+int main() {
+    ExprNode node(NODE_ERR);
+    std::string inp = "x * x + y * y < 200";
+    getExpr(inp, node);
+    std::vector<std::string> dims = {"x", "y"};
+    GridDomain domain(1.0, node, dims);
+    std::vector<double> init = {0.0, 0.0};
+    domain.findDomain(init);
+    std::cout << domain.str();
     return 0;
 }
