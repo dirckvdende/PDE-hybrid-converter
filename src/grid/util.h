ef42ced44561f2d431b2cdeda32be0d415170e24
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-10 19:56:47 +0200

    Added grid util files

---
+++ src/grid/util.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+/**
+ * Convert a grid position to a string
+ * @param pos The grid position as a vector
+ * @return A string representation of the position
+ */
+std::string gridToString(const std::vector<size_t> &pos);
