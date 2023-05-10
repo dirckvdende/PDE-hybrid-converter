928a85d2c9ee79b2d9c39ffa689d1dfa52d25fef
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-10 10:44:36 +0200

    Added testing code

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,14 @@
 
-#include "expr/expr.h"
+#include "expr/lexer.h"
+#include "expr/parser.h"
 #include <iostream>
 
 int main() {
-    ExprNode a(NODE_NUM);
-    a.content = "5";
-    ExprNode b = a;
-    b.content = "7";
-    ExprNode sm(NODE_ADD);
-    sm[0] = a, sm[1] = b;
-    std::cout << sm.str() << std::endl;
+    std::string inp = "5 + 7 * 8";
+    ExprLexer lexer(inp);
+    lexer.run();
+    ExprParser parser(lexer.getTokens());
+    parser.run();
+    std::cout << parser.getTree().str() << std::endl;
     return 0;
 }
