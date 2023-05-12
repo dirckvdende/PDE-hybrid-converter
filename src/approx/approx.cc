2bc2df0aff61be8f6f83ef657532464330ff6efd
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-12 13:57:17 +0200

    Added coefficient calculation function

--- src/approx/approx.cc
+++ src/approx/approx.cc
@@ -14,3 +14,30 @@ ExprNode SpatialApprox::getApprox(std::string func, const std::vector<size_t>
         amounts[dimMap[d]]++;
     // TODO: implement
 }
+
+std::vector<double> SpatialApprox::getCoeffs(size_t n, double scale) {
+    if (n % 2 == 0) {
+        std::vector<double> coeffs(n + 1);
+        // c_i = (-1)^(n-i) (n choose i) / scale^n
+        double c = 1.0;
+        if (n % 2 != 0)
+            c = -1.0;
+        for (size_t i = 0; i < n; i++)
+            c /= scale;
+        for (size_t i = 0; i <= n; i++) {
+            coeffs[i] = c;
+            c = c / (i + 1) * (n - i);
+        }
+        return coeffs;
+    }
+    std::vector<double> prev = getCoeffs(n - 1, scale);
+    std::vector<double> coeffs(n + 2, 0.0);
+    for (size_t i = 0; i <= n + 1; i++) {
+        if (i <= n - 1)
+            coeffs[i] -= prev[i];
+        if (i >= 2)
+            coeffs[i] += prev[i - 2];
+        coeffs[i] /= scale;
+    }
+    return coeffs;
+}
