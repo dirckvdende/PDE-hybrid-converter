b904f4ce22708d8f47300f1a7c36d2760f5e722a
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-14 11:24:30 +0200

    Added str() method to InputFields

--- src/parser/fields.cc
+++ src/parser/fields.cc
@@ -1,2 +1,30 @@
 
 #include "fields.h"
+#include <string>
+
+std::string InputFields::str() const {
+    std::string out;
+    out += "iterations -> " + std::to_string(iterations) + "\n";
+    out += "time       -> " + std::to_string(time) + "\n";
+    out += "init       -> " + init.str() + "\n";
+    out += "boundary   -> " + boundary.str() + "\n";
+    out += "scale      -> " + std::to_string(scale) + "\n";
+    out += "pivot      -> [";
+    for (size_t i = 0; i < pivot.size(); i++) {
+        if (i != 0)
+            out += ", ";
+        out += std::to_string(pivot[i]);
+    }
+    out += "]\n";
+    out += "domain     -> " + domain.str() + "\n";
+    out += "func       -> " + func.str() + "\n";
+    out += "equation   -> " + equation.str() + "\n";
+    out += "dimensions -> [";
+    for (size_t i = 0; i < dimensions.size(); i++) {
+        if (i != 0)
+            out += ", ";
+        out += "\"" + dimensions[i] + "\"";
+    }
+    out += "]\n";
+    return out;
+}
