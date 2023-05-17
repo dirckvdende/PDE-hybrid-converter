a1c449bcd4c84f027d0ce3fec72fd0ae4f87bac4
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-17 15:55:10 +0200

    Replaced main with testing code

--- src/main.cc
+++ src/main.cc
@@ -1,9 +1,22 @@
 
 #include "compiler/compiler.h"
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+#include "sim/sim.h"
 
 int main(int argc, char *argv[]) {
-    Compiler compiler(argc, argv);
-    compiler.run();
+    // Compiler compiler(argc, argv);
+    // compiler.run();
+    // return 0;
+    sim::Sim simulator;
+    simulator.setFileOutput(true);
+    std::ifstream file("examples/ode/single.ode");
+    std::stringstream stream;
+    stream << file.rdbuf();
+    std::string txt = stream.str();
+    simulator.setText(txt);
+    simulator.run();
     return 0;
 }
