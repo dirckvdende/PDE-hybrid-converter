1953ede5c243bc0a2839457f4fe46c353e09e9d4
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-17 19:43:54 +0200

    Added script for visual output of ODEs

---
+++ src/sim/graphic.py
@@ -0,0 +1,16 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: " + sys.argv[0] + " <filename>")
+    exit()
+os.system("./build/main --ode " + sys.argv[1])
+f = open("./tmp/ode.txt")
+
+for line in f.readlines():
+    data = line.strip().split(" ")
+    plt.plot([float(x) for x in data[1:]])
+plt.savefig("tmp/ode.png")
