a5583fbe4eb0a80c35e4360adf332584946fea5c
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-17 10:59:47 +0200

    Implemented main functions of sim

--- src/sim/sim.cc
+++ src/sim/sim.cc
@@ -1,9 +1,12 @@
 
 #include "sim.h"
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 using namespace sim;
 
-Sim::Sim() { }
+Sim::Sim() : specs(parser.getSpecs()), fileOutput(false), stepSize(1e-2) { }
 
 Sim::~Sim() { }
 
@@ -13,5 +16,79 @@ void Sim::setText(const std::string &txt) {
 }
 
 void Sim::run() {
+    runParser();
+    for (const ODESystem &system : specs)
+        runSystem(system);
+    if (fileOutput)
+        outputEmit("tmp/ode.txt");
+}
+
+void Sim::setFileOutput(bool val) {
+    fileOutput = val;
+}
+
+void Sim::setStepSize(bool val) {
+    stepSize = val;
+}
+
+void Sim::runParser() {
+    parser.run();
+}
+
+void Sim::runSystem(const ODESystem &system) {
+    // Map from variables to index in the ODE system vars list
+    std::unordered_map<std::string, size_t> names;
+    for (size_t i = 0; i < system.vars.size(); i++) {
+        if (names.find(system.vars[i]) != names.end())
+            throw std::runtime_error("Duplicate variable name in system");
+        names[system.vars[i]] = i;
+    }
+    // Determine initial values
+    std::vector<double> vals(system.vals.size());
+    for (size_t i = 0; i < system.vals.size(); i++) {
+        if (system.vals[i].type == expr::NODE_INTEG) {
+            vals[i] = system.vals[i][1].eval();
+        } else {
+            expr::ExprNode node = system.vals[i];
+            for (size_t j = 0; j < i; j++) {
+                expr::ExprNode search(expr::NODE_SYMB, {}, system.vars[j]);
+                expr::ExprNode repl(expr::NODE_NUM, {},
+                std::to_string(vals[j]));
+                node.replace(search, repl);
+            }
+        }
+    }
+    // Determine variables that need to be emitted
+    std::vector<std::pair<size_t, std::string>> emits;
+    for (const auto &emit : system.emit) {
+        emits.push_back({names[emit.first], emit.second});
+        emitVals[emit.second] = {};
+    }
+    // Run simulation
+    for (double t = 0; t < system.time; t += stepSize) {
+        for (const auto &emit : emits)
+            emitVals[emit.second].push_back(vals[emit.first]);
+        for (size_t i = 0; i < system.vals.size(); i++) {
+            expr::ExprNode node;
+            if (system.vals[i].type == expr::NODE_INTEG)
+                node = system.vals[i][0];
+            else
+                node = system.vals[i];
+            for (size_t j = 0; j < system.vals.size(); j++) {
+                expr::ExprNode search(expr::NODE_SYMB, {}, system.vars[j]);
+                expr::ExprNode repl(expr::NODE_NUM, {},
+                std::to_string(vals[j]));
+                node.replace(search, repl);
+            }
+            double val = node.eval();
+            if (system.vals[i].type == expr::NODE_INTEG)
+                vals[i] += stepSize * val;
+            else
+                vals[i] = val;
+        }
+    }
+}
+
+void Sim::outputEmit(std::string filename) const {
     // TODO: implement
 }
--- src/sim/sim.h
+++ src/sim/sim.h
@@ -4,6 +4,7 @@
 #include "parser.h"
 #include "spec.h"
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace sim {
@@ -36,14 +37,49 @@ public:
      */
     void run();
 
+    /**
+     * Set the file output switch
+     * @param val The new value
+     */
+    void setFileOutput(bool val);
+
+    /**
+     * Set the simulation step size
+     * @param val The new value
+     */
+    void setStepSize(bool val);
+
 private:
+
+    /**
+     * Run parser
+     */
+    void runParser();
+
+    /**
+     * Simulate a specific ODE system
+     * @param system The system specification
+     */
+    void runSystem(const ODESystem &system);
+
+    /**
+     * Output the emit values to a file
+     * @param filename The name of the file to output to
+     */
+    void outputEmit(std::string filename) const;
 
     // Input text
     std::string txt;
     // Parses the input text
     Parser parser;
     // List of ODE specifications to process
-    std::vector<ODESystem> specs;
+    const std::vector<ODESystem> &specs;
+    // Indicates if the results should be output to a text file
+    bool fileOutput;
+    // Simulation step size
+    double stepSize;
+    // "Emit" values stored after simulating the ODEs
+    std::unordered_map<std::string, std::vector<double>> emitVals;
 
 };
 
