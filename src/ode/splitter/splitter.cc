35d36e2c68fc4e2ddbb07d443e7356c0aaa0bbe9
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-25 10:20:49 +0200

    Added some functions to splitter

--- src/ode/splitter/splitter.cc
+++ src/ode/splitter/splitter.cc
@@ -1,8 +1,27 @@
 
 #include "splitter.h"
 
+using namespace ode;
 using namespace ode::splitter;
 
 Splitter::Splitter() { }
 
 Splitter::~Splitter() { }
+
+void Splitter::setSystems(const std::vector<ODESystem> &val) {
+    systems = val;
+}
+
+void Splitter::run() {
+    outputSystems.clear();
+    for (const ODESystem &system : systems)
+        splitSystem(system);
+}
+
+const std::vector<ODESystem> &Splitter::getOutputSystems() const {
+    return outputSystems;
+}
+
+void Splitter::splitSystem(const ODESystem &system) {
+    // TODO: implement
+}
