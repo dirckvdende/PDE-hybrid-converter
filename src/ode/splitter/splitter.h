7ee9cb7de47cb05bec9b8d7bcea9eaa4cbdb09a6
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-25 10:20:49 +0200

    Added some functions to splitter

--- src/ode/splitter/splitter.h
+++ src/ode/splitter/splitter.h
@@ -1,6 +1,9 @@
 
 #pragma once
 
+#include "ode/spec.h"
+#include <vector>
+
 namespace ode::splitter {
 
 /**
@@ -20,7 +23,36 @@ public:
      */
     ~Splitter();
 
+    /**
+     * Set the list of input systems to split
+     * @param val The list of input systems
+     */
+    void setSystems(const std::vector<ODESystem> &val);
+
+    /**
+     * Run the splitter
+     */
+    void run();
+
+    /**
+     * Get the list of output systems (splitted input), assumes run() has been
+     * called
+     * @return A constant reference to the list of output systems
+     */
+    const std::vector<ODESystem> &getOutputSystems() const;
+
 private:
+
+    /**
+     * Split a single system, and append result to output systems list
+     * @param system The system to split
+     */
+    void splitSystem(const ODESystem &system);
+
+    // List of input systems
+    std::vector<ODESystem> systems;
+    // List of output systems
+    std::vector<ODESystem> outputSystems;
 
 };
 
