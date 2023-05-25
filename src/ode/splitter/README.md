aa56339b98cefafb841f2de0c07e762e19f7d397
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-25 10:11:17 +0200

    Added splitter folder

---
+++ src/ode/splitter/README.md
@@ -0,0 +1,4 @@
+
+# ODE splitter
+
+This tool can split up a list of ODE systems based on the dependencies between the variables.
---
+++ src/ode/splitter/splitter.h
@@ -0,0 +1,27 @@
+
+#pragma once
+
+namespace ode::splitter {
+
+/**
+ * Main object to split up systems of ODEs
+ */
+class Splitter {
+
+public:
+
+    /**
+     * Constructor
+     */
+    Splitter();
+
+    /**
+     * Destructor
+     */
+    ~Splitter();
+
+private:
+
+};
+
+}
