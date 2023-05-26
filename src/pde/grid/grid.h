3ca51f5170c6c52e97925e4f0b97d2ef92238f01
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-26 19:47:38 +0200

    Expanded grid and pdegrid definitions

--- src/pde/grid/grid.h
+++ src/pde/grid/grid.h
@@ -1,13 +1,17 @@
 
 #pragma once
 
+#include "cell.h"
+#include "hypergrid/hypergrid.h"
+#include <vector>
+
 namespace pde::grid {
 
 /**
  * Basic grid that stores information about what the several components in the
  * compiler have generated
  */
-class Grid {
+class Grid : public hypergrid::HyperGrid<GridCell> {
 
 public:
 
@@ -21,7 +25,25 @@ public:
      */
     virtual ~Grid();
 
+    /**
+     * Set the pivot position of the grid
+     * @param val The pivot position
+     */
+    void setPivot(const std::vector<double> &val);
+
+    /**
+     * Set the scale of the grid
+     * @param val The new grid cell scale
+     */
+    void setScale(double val);
+
 private:
+
+    // Pivot point of the grid, this indicates the coordinates of the 0 integer
+    // point
+    std::vector<double> pivot;
+    // The scale of the grid
+    double scale;
 
 };
 
