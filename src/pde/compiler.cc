7625b72e51340ec1713374bc8143c84446c50e94
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-27 15:14:41 +0200

    Added compiler component

---
+++ src/pde/compiler.cc
@@ -0,0 +1,25 @@
+
+#include "compiler.h"
+#include "grid/pdegrid.h"
+#include "parser/parser.h"
+#include "parser/spec.h"
+#include <string>
+
+using namespace pde;
+
+Compiler::Compiler() { }
+
+Compiler::~Compiler() { }
+
+void Compiler::setInput(const std::string &val) {
+    txt = val;
+}
+
+void Compiler::run() {
+    parser.setText(txt);
+    parser.run();
+    system = parser.getSpecs();
+    grid.setSystem(system);
+    grid.generate();
+    // TODO: implement
+}
