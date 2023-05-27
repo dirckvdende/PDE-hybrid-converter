53e04b93af2ab6ed98e0bd04d10b4cc45ff5ae68
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-27 14:09:59 +0200

    Added more test programs for groups

---
+++ tests/groups/graphic.py
@@ -0,0 +1,33 @@
+#
+# Visualize a 2D grid where grid cells are in groups
+# Each group will have some color
+#
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: " + sys.argv[0] + " <input file>")
+    exit()
+
+data = []
+with open(sys.argv[1]) as f:
+    for line in f:
+        data.append([])
+        for entry in line.split(" ")[:-1]:
+            if entry == "":
+                continue
+            data[-1].append(int(entry))
+colorMap = np.random.random(len(data) * len(data[0]))
+isBorder = "_border" in sys.argv[1]
+if not isBorder:
+    for x in range(len(data)):
+        for y in range(len(data[0])):
+            data[x][y] = colorMap[data[x][y]]
+
+if isBorder:
+    plt.imshow(data, interpolation="nearest", cmap="Greys")
+else:
+    plt.imshow(data, interpolation="nearest")
+plt.savefig(sys.argv[1].replace(".txt", ".png"), dpi=200, bbox_inches="tight")
---
+++ tests/groups/visual.cc
@@ -0,0 +1,88 @@
+/**
+ * This test outputs group divisions for each algorithm to a file to a given
+ * folder where text files will be placed
+ * 
+ * This program should be executed from the root folder of this project!
+ */
+
+#include "pde/grid/groups/alg/alg.h"
+#include "pde/grid/groups/alg/optimalrect.h"
+#include "pde/grid/groups/alg/random.h"
+#include "pde/grid/groups/alg/spread.h"
+#include "pde/grid/groups/alg/squares.h"
+#include "pde/grid/groups/alg/rectspread.h"
+#include "pde/grid/groups/depgrid.h"
+#include "tests/groups.h"
+#include <fstream>
+#include <iostream>
+
+std::string outputFolder;
+
+/**
+ * Object used produce visual ouput of algorithms
+ */
+class TestVisual final : public tests::groups::TestRunner {
+
+public:
+
+    /**
+     * Constructor
+     * @param dims The dimensions of the grid
+     * @param range The dependency range of the grid
+     * @param maxSize The maximum group size of the grid
+     */
+    TestVisual(std::vector<size_t> dims, std::vector<size_t> range, size_t
+    maxSize) {
+        this->dims = dims;
+        this->range = range;
+        this->maxSize = maxSize;
+    }
+
+    /**
+     * Destructor
+     */
+    virtual ~TestVisual() { };
+
+    /**
+     * Run a specific test case, the grid has already been prepared
+     * @param alg The division algorithm to use
+     * @return A boolean indicating if the test was successful
+     */
+    virtual bool run(pde::grid::groups::alg::DivisionAlg *alg) {
+        // Run algorithm and determine dependencies
+        alg->run();
+        grid->calc();
+        // Output groups
+        std::ofstream file(outputFolder + "/" + names[alg] + ".txt");
+        if (!file.is_open())
+            exit(1);
+        file << ((pde::grid::groups::GroupGrid *)grid)->str();
+        file.close();
+        // Output border
+        std::ofstream borderFile(outputFolder + "/" + names[alg] + "_border.txt");
+        if (!borderFile.is_open())
+            exit(1);
+        borderFile << grid->str();
+        borderFile.close();
+        // Run visualization program
+        system(("python3 tests/groups/graphic.py " + outputFolder + "/" +
+        names[alg] + ".txt").c_str());
+        system(("python3 tests/groups/graphic.py " + outputFolder + "/" +
+        names[alg] + "_border.txt").c_str());
+        return true;
+    }
+
+};
+
+int main(int argc, char *argv[]) {
+    outputFolder = "tmp";
+    TestVisual test({100, 100}, {1, 1}, 121);
+    test.setConfig(argc, argv);
+    test.add<pde::grid::groups::alg::RandomAlg>("RandomAlg");
+    test.add<pde::grid::groups::alg::SquaresAlg>("SquaresAlg");
+    test.add<pde::grid::groups::alg::OptimalRectAlg>("OptimalRectAlg");
+    test.add<pde::grid::groups::alg::SpreadAlg>("SpreadAlg");
+    test.add<pde::grid::groups::alg::RectSpreadAlg>("RectSpreadAlg");
+    test.runAll();
+    return 0;
+}
