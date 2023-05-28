b299abb4934e3a5e974f24b71b387677bad15362
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-28 12:24:12 +0200

    Added start of conversion to ODEs

--- src/pde/compiler.cc
+++ src/pde/compiler.cc
@@ -1,8 +1,10 @@
 
 #include "compiler.h"
+#include "grid/cell.h"
 #include "grid/pdegrid.h"
 #include "parser/parser.h"
 #include "parser/spec.h"
+#include <fstream>
 #include <string>
 
 using namespace pde;
@@ -23,5 +25,27 @@ void Compiler::run() {
     grid.setSystem(system);
     grid.setComponentLimit(168);
     grid.generate();
-    // TODO: implement
+    generateODE();
+    // TODO: remove this when iterations are implemented properly
+    std::ofstream file("tmp/compiler.ode");
+    file << ode.str();
+    file.close();
+    // TODO: implement iterations
+}
+
+void Compiler::generateODE() {
+    // TODO: implement iterations
+    for (grid::GridCell &cell : grid) {
+        if (cell.type == grid::CELL_BORDER || cell.type == grid::CELL_DOMAIN) {
+            ode.vars.insert(ode.vars.begin(), cell.vars.begin(),
+            cell.vars.end());
+            ode.vals.insert(ode.vals.end(), cell.vals.begin(), cell.vals.end());
+            if (cell.isStored)
+                for (const std::string &var : cell.vars)
+                    ode.emit.push_back({var, var});
+            ode.bounds.insert(ode.bounds.begin(), system.bounds.begin(),
+            system.bounds.end());
+        }
+    }
+    ode.time = system.time;
 }
