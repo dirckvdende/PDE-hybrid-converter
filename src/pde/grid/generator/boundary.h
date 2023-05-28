4acb6e5a9744bb33bb716b7bebacdc52d9c63a9b
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-28 11:29:28 +0200

    Implemented boundary expression generation

--- src/pde/grid/generator/boundary.h
+++ src/pde/grid/generator/boundary.h
@@ -4,9 +4,13 @@
 #include "generator.h"
 #include "pde/grid/cell.h"
 #include "pde/grid/grid.h"
+#include "pde/parser/spec.h"
 
 namespace pde::grid::generator {
 
+/**
+ * This object can generate expressions for boundary cells on the grid
+ */
 class BoundaryExprGenerator final : public ExprGenerator {
 
 public:
@@ -20,6 +24,13 @@ public:
      * Destructor
      */
     ~BoundaryExprGenerator();
+
+    /**
+     * Set the system that the user entered. This function expands the parent
+     * definition to also do preprocessing of the boundary expressions
+     * @param sys The system to save in this generator
+     */
+    virtual void setSystem(const parser::PDESystem &sys);
 
     /**
      * Generate the expression for a given grid cell
