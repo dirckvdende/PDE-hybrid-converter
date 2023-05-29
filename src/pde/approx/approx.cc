c443c04e09cd672e6267c0edd588c2b69d18675f
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-29 11:57:54 +0200

    Added spatial approx class

--- src/pde/approx/approx.cc
+++ src/pde/approx/approx.cc
@@ -1,4 +1,89 @@
 
 #include "approx.h"
+#include "pde/grid/generator/util.h"
 
+using namespace pde;
 using namespace pde::approx;
+
+SpatialApprox::SpatialApprox() : grid(nullptr), curCell(nullptr), iteration(0)
+{ }
+
+SpatialApprox::~SpatialApprox() { }
+
+void SpatialApprox::setScale(double val) {
+    scale = val;
+}
+
+void SpatialApprox::setDims(const std::vector<std::string> &dims) {
+    dimMap.clear();
+    for (size_t i = 0; i < dims.size(); i++)
+        dimMap.emplace(dims[i], i);
+}
+
+void SpatialApprox::setGrid(grid::Grid &gridRef) {
+    grid = &gridRef;
+}
+
+void SpatialApprox::setIteration(size_t val) {
+    iteration = val;
+}
+
+void SpatialApprox::convert() {
+    for (grid::GridCell &cell : *grid)
+        convert(cell);
+}
+
+void SpatialApprox::convert(grid::GridCell &cell) {
+    curCell = &cell;
+    for (expr::ExprNode &node : cell.vals)
+        replaceApprox(node);
+    curCell = nullptr;
+}
+
+std::vector<double> SpatialApprox::getCoeffs(size_t n) {
+    if (n % 2 == 0) {
+        std::vector<double> coeffs(n + 1);
+        // c_i = (-1)^(n-i) (n choose i) / scale^n
+        double c = 1.0;
+        if (n % 2 != 0)
+            c = -1.0;
+        for (size_t i = 0; i < n; i++)
+            c /= scale;
+        for (size_t i = 0; i <= n; i++) {
+            coeffs[i] = c;
+            c = c / (i + 1) * (n - i);
+        }
+        return coeffs;
+    }
+    std::vector<double> prev = getCoeffs(n - 1);
+    std::vector<double> coeffs(n + 2, 0.0);
+    for (size_t i = 0; i <= n + 1; i++) {
+        if (i <= n - 1)
+            coeffs[i] -= prev[i];
+        if (i >= 2)
+            coeffs[i] += prev[i - 2];
+        coeffs[i] /= scale;
+    }
+    return coeffs;
+}
+
+void SpatialApprox::replaceApprox(expr::ExprNode &node) const {
+    if (node.type == expr::NODE_DERIV) {
+        node = getApprox(node);
+        return;
+    }
+    for (expr::ExprNode *child : node.children)
+        replaceApprox(*child);
+}
+
+expr::ExprNode SpatialApprox::getApprox(const expr::ExprNode &node) const {
+    // TODO: implement
+    if (node.deriv.dims != std::vector<std::string>{"x", "x"})
+        throw std::runtime_error("not implemented");
+    std::vector<size_t> cur = grid->toLoc(curCell);
+    cur[0]--;
+    expr::ExprNode *out = new expr::ExprNode(expr::NODE_ADD, {
+        
+    }, grid::generator::toGridVar(node.deriv.var, cur, iteration));
+    
+}
--- src/pde/approx/approx.h
+++ src/pde/approx/approx.h
@@ -1,8 +1,100 @@
 
 #pragma once
 
+#include "expr/expr.h"
+#include "pde/grid/cell.h"
+#include "pde/grid/grid.h"
+#include <unordered_map>
+#include <vector>
+
 namespace pde::approx {
 
+/**
+ * Object used to approximate spatial derivatives
+ */
+class SpatialApprox {
 
+public:
+
+    /**
+     * Constructor
+     */
+    SpatialApprox();
+
+    /**
+     * Destructor
+     */
+    ~SpatialApprox();
+
+    /**
+     * Store the scale of the grid in this object
+     * @param val The scale of the grid
+     */
+    void setScale(double val);
+
+    /**
+     * Set the names of the dimensions
+     * @param dims A list of the names of the dimensions, in order
+     */
+    void setDims(const std::vector<std::string> &dims);
+
+    /**
+     * Set the grid on which all of the cells to be processed are present
+     * @param gridRef A reference to the grid
+     */
+    void setGrid(grid::Grid &gridRef);
+
+    /**
+     * Set the current iteration
+     * @param val The new value for the current iteration
+     */
+    void setIteration(size_t val);
+
+    /**
+     * Convert all spatial derivatives in the stored grid
+     */
+    void convert();
+
+private:
+
+    /**
+     * Convert all spatial derivatives in the given grid cell
+     * @param cell A reference to the cell in which to convert all derivatives
+     */
+    void convert(grid::GridCell &cell);
+
+    /**
+     * Get the coefficients of a one-dimensional approximation of an n-th
+     * derivative
+     * @param n The n-th derivative is approximated
+     * @return The list of coefficients
+     */
+    std::vector<double> getCoeffs(size_t n);
+
+    /**
+     * Replace all derivatives in an expression node with approximations
+     * @param node A reference to the root node
+     */
+    void replaceApprox(expr::ExprNode &node) const;
+
+    /**
+     * Convert a derivative expression node to an approximation
+     * @param node A constant reference to the derivative node to convert
+     * @return The converted node
+     */
+    expr::ExprNode getApprox(const expr::ExprNode &node) const;
+
+    // Grid scale
+    double scale;
+    // Map from dimension names to dimension indices
+    std::unordered_map<std::string, size_t> dimMap;
+    // The grid on which all of the cells are defined
+    grid::Grid *grid;
+    // Keep track of the current cell being processed
+    grid::GridCell *curCell;
+    // Current iteration
+    size_t iteration;
+
+};
 
 }
