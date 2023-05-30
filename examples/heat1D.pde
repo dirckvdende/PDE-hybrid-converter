64cd6c63108d1ac9c6c9725dcf0f57e14b3ecd5d
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-30 10:02:05 +0200

    Added temporary implementation of approximations

--- examples/heat1D.pde
+++ examples/heat1D.pde
@@ -1,9 +1,9 @@
 
 pde {
     dims [x];
-    domain x * x < 100;
+    domain x * x < 1;
     pivot [0, 0];
-    scale 0.1;
+    scale 0.4;
 
     equation dt(u) = dx(dx(u));
     init u = 1 - x * x / 2 + x * x * x * x / 24;
--- src/pde/grid/generator/internal.cc
+++ src/pde/grid/generator/internal.cc
@@ -1,5 +1,7 @@
 
 #include "internal.h"
+#include "util.h"
+#include <stack>
 
 using namespace pde::grid::generator;
 
@@ -12,10 +14,58 @@ void InternalExprGenerator::generate(GridCell &cell) {
         cell.vals.push_back(system.vals[i]);
         cell.vals.back().replaceDirect(loc);
     }
+    replaceAll(cell);
 }
 
-void InternalExprGenerator::generateApprox(GridCell &cell, const
-std::vector<size_t> &derivs) {
-    // TODO: implement
-    (void)cell; (void)derivs;
+void InternalExprGenerator::replaceAll(GridCell &cell) {
+    genDimMap();
+    for (expr::ExprNode &val : cell.vals) {
+        std::stack<expr::ExprNode *> children;
+        children.push(&val);
+        while (!children.empty()) {
+            expr::ExprNode *cur = children.top();
+            children.pop();
+            if (cur->type == expr::NODE_DERIV) {
+                std::vector<size_t> amt(grid.dims.size(), 0);
+                for (const std::string &dim : cur->deriv.dims)
+                    amt[dimMap[dim]]++;
+                *cur = generateApprox(cell, amt, cur->deriv.var);
+            } else {
+                for (expr::ExprNode *child : cur->children)
+                    children.push(child);
+            }
+        }
+    }
+}
+
+expr::ExprNode InternalExprGenerator::generateApprox(GridCell &cell, const
+std::vector<size_t> &deriv, const std::string &name) {
+    // TODO: implement properly
+    std::vector<size_t> mid = grid.toLoc(cell);
+    std::vector<size_t> left = mid, right = mid;
+    left[0]--;
+    right[0]++;
+    return expr::ExprNode(expr::NODE_ADD, {
+        new expr::ExprNode(expr::NODE_ADD, {
+            new expr::ExprNode(expr::NODE_MUL, {
+                new expr::ExprNode(expr::NODE_NUM, {}, 1.0),
+                new expr::ExprNode(expr::NODE_SYMB, {}, toGridVar(name, left, grid.iteration)),
+            }),
+            new expr::ExprNode(expr::NODE_MUL, {
+                new expr::ExprNode(expr::NODE_NUM, {}, -2.0),
+                new expr::ExprNode(expr::NODE_SYMB, {}, toGridVar(name, mid, grid.iteration)),
+            })
+        }),
+        new expr::ExprNode(expr::NODE_MUL, {
+            new expr::ExprNode(expr::NODE_NUM, {}, 1.0),
+            new expr::ExprNode(expr::NODE_SYMB, {}, toGridVar(name, right, grid.iteration)),
+        })
+    });
+}
+
+void InternalExprGenerator::genDimMap() {
+    if (!dimMap.empty())
+        return;
+    for (size_t i = 0; i < grid.dims.size(); i++)
+        dimMap.emplace(grid.dims[i], i);
 }
