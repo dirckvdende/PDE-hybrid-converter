19fc54e82996680ba5f2e9427e5128b05e878f94
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-05-31 19:44:43 +0200

    Added code to generate initial guess

--- src/pde/ode.cc
+++ src/pde/ode.cc
@@ -19,19 +19,20 @@ void ODEGenerator::changeSettings(const CompilerSettings &val) {
 }
 
 void ODEGenerator::run() {
-    grid::GridGenerator gen;
     gen.setSystem(pde);
     gen.grid.maxGridSize = settings.maxGridSize;
     gen.grid.componentLimit = settings.componentLimit;
     gen.prepare();
-    for (size_t iteration = 0; iteration < pde.iterations; iteration++) {
+    generateInitIteration();
+    for (size_t iteration = 1; iteration <= pde.iterations; iteration++) {
         gen.run(iteration);
-        if (iteration == 0 && hasTimeReference(gen.grid))
+        if (iteration == 1 && hasTimeReference(gen.grid))
             addTimeSystem();
         systems.emplace_back();
         ode::ODESystem &ode = systems.back();
         for (grid::GridCell &cell : gen.grid) {
-            if (cell.type == grid::CELL_BORDER || cell.type == grid::CELL_DOMAIN) {
+            if (cell.type == grid::CELL_BORDER || cell.type ==
+            grid::CELL_DOMAIN) {
                 ode.vars.insert(ode.vars.end(), cell.vars.begin(),
                 cell.vars.end());
                 if (cell.type == grid::CELL_DOMAIN) {
@@ -48,7 +49,7 @@ void ODEGenerator::run() {
                 if (cell.isStored)
                     for (const std::string &var : cell.vars)
                         ode.emit.push_back({var, var});
-                ode.bounds.insert(ode.bounds.begin(), pde.bounds.begin(),
+                ode.bounds.insert(ode.bounds.end(), pde.bounds.begin(),
                 pde.bounds.end());
             }
         }
@@ -79,4 +80,31 @@ void ODEGenerator::addTimeSystem() {
     system.bounds = {{0.0, pde.time}};
     system.time = pde.time;
     systems.push_back(system);
+}
+
+void ODEGenerator::generateInitIteration() {
+    gen.run(0);
+    systems.emplace_back();
+    ode::ODESystem &ode = systems.back();
+    for (grid::GridCell &cell : gen.grid) {
+        if (cell.type == grid::CELL_DOMAIN && cell.isStored) {
+            ode.vars.insert(ode.vars.end(), cell.vars.begin(),
+            cell.vars.end());
+            if (cell.type == grid::CELL_DOMAIN) {
+                for (size_t i = 0; i < cell.init.size(); i++)
+                    ode.vals.push_back(expr::ExprNode(expr::NODE_NUM, {},
+                    cell.init[i]));
+            } else {
+                ode.vals.insert(ode.vals.end(), cell.vals.begin(),
+                cell.vals.end());
+            }
+            for (const std::string &var : cell.vars)
+                ode.emit.push_back({var, var});
+            ode.bounds.insert(ode.bounds.end(), pde.bounds.begin(),
+            pde.bounds.end());
+        }
+    }
+    ode.time = pde.time;
+    if (ode.vals.empty())
+        systems.pop_back();
 }
