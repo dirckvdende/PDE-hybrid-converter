0017a4bc6fc8106f93db718d1f73f93dadc99408
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-06-02 17:52:24 +0200

    Added a data class for the ODE sim

---
+++ src/ode/sim/data.cc
@@ -0,0 +1,57 @@
+
+#include "data.h"
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace ode::sim;
+
+SimData::SimData() { }
+
+SimData::~SimData() { }
+
+void SimData::add(const std::string &name) {
+    size_t index;
+    if (emitNames.find(name) != emitNames.end()) {
+        index = emitNames.at(name);
+        emitNames.erase(name);
+        data[index].values.clear();
+    } else {
+        index = data.size();
+        data.emplace_back();
+    }
+    names.emplace(name, index);
+}
+
+double SimData::get(size_t index, size_t step) const {
+    data[index].values[step];
+}
+
+size_t SimData::size(size_t index) const {
+    return data[index].values.size();
+}
+
+size_t SimData::getIndex(const std::string &name) const {
+    if (names.find(name) != names.end())
+        return names.at(name);
+    return emitNames.at(name);
+}
+
+void SimData::append(size_t index, double val) {
+    data[index].values.push_back(val);
+}
+
+void SimData::reset() {
+    for (DataEntry &entry : data)
+        if (entry.emitRefs == 0)
+            entry.values.clear();
+    names.clear();
+}
+
+void SimData::addEmit(const std::string &inp, const std::string &out) {
+    size_t index = names.at(inp);
+    if (emitNames.find(out) != emitNames.end())
+        data[emitNames.at(out)].emitRefs--;
+    emitNames.emplace(out, index);
+    data[index].emitRefs++;
+}
---
+++ src/ode/sim/data.h
@@ -0,0 +1,96 @@
+
+#pragma once
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace ode::sim {
+
+/**
+ * Object used to keep track of variable names and emit variables
+ */
+class SimData {
+
+public:
+
+    /**
+     * Constructor
+     */
+    SimData();
+
+    /**
+     * Destructor
+     */
+    ~SimData();
+
+    /**
+     * Add a variable to the dataframe. If the variable already exists, it will
+     * be reset
+     * @param name The name of the variable
+     */
+    void add(const std::string &name);
+
+    /**
+     * Get data at a specific index of the data frame
+     * @param index The index in the data frame
+     * @param step The time step at which the data should be retrieved
+     * @return A double with the 
+     */
+    double get(size_t index, size_t step) const;
+
+    /**
+     * Get the number of time steps recorded for a specific index
+     * @param index The index in the data frame
+     * @return The number of time steps recorded
+     */
+    size_t size(size_t index) const;
+
+    /**
+     * Get the index of a variable in the data frame
+     * @param name The name of the variable
+     * @return The index of the variable
+     */
+    size_t getIndex(const std::string &name) const;
+
+    /**
+     * Append data to a speicifc index in the data frame
+     * @param index The index at which to append
+     * @param val The value to append
+     */
+    void append(size_t index, double val);
+
+    /**
+     * Reset data frame variables that are not emit variables
+     */
+    void reset();
+
+    /**
+     * Add an emit to the dataframe
+     * @param inp Input variable name
+     * @param out Output variable name
+     */
+    void addEmit(const std::string &inp, const std::string &out);
+
+private:
+
+    /**
+     * Data frame entry
+     */
+    struct DataEntry {
+        // Entry has one or more emit references
+        size_t emitRefs = 0;
+        // Data values
+        std::vector<double> values;
+    };
+
+    // Map from variable names to indices in the data frame
+    std::unordered_map<std::string, size_t> names;
+    // Emit variable indices
+    std::unordered_map<std::string, size_t> emitNames;
+    // Data frame data
+    std::vector<DataEntry> data;
+
+};
+
+}
