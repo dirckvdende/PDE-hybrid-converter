34385b481fee6d5d93c286b994cb9adb4c0a5fbf
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-06-07 12:43:19 +0200

    Added check class and files

---
+++ src/pde/parser/checker.h
@@ -0,0 +1,28 @@
+
+#pragma once
+
+namespace pde::parser {
+
+/**
+ * Object used to validate input format. This is done after the tree parser is
+ * run
+ */
+class InputValidator {
+
+public:
+
+    /**
+     * Constructor
+     */
+    InputValidator();
+
+    /**
+     * Destructor
+     */
+    ~InputValidator();
+
+private:
+
+};
+
+}
