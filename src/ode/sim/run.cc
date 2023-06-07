c67ea41363b2b2951b177ce8555602f658b3e2d6
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-06-07 12:30:52 +0200

    Added help page for ODE sim

--- src/ode/sim/run.cc
+++ src/ode/sim/run.cc
@@ -8,9 +8,27 @@
 
 using namespace ode::sim;
 
+bool ode::sim::checkHelp(int argc, char *argv[]) {
+    for (int i = 1; i < argc; i++)
+        if (argv[i] == std::string("-h"))
+            return true;
+    return false;
+}
+
+void ode::sim::showHelp() {
+    std::cout <<
+        "ODE specification simulator. Takes as an input an ODE specification\n"
+        "file, and outputs data from the \"emit\" variables to a CSV file\n"
+        "\"tmp/ode.csv\"." << std::endl;
+}
+
 int ode::sim::run(int argc, char *argv[]) {
     std::string filename;
     ode::sim::Sim simulator;
+    if (checkHelp(argc, argv)) {
+        showHelp();
+        return 0;
+    }
     for (int i = 1; i < argc; i++) {
         std::string arg = argv[i];
         if (arg.size() >= 1 && arg.front() != '-')
--- src/ode/sim/run.h
+++ src/ode/sim/run.h
@@ -4,6 +4,19 @@
 namespace ode::sim {
 
 /**
+ * Check if help page needs to be shown, by checking for "-h" argument
+ * @param argc Number of command line arguments
+ * @param argv The command line arguments
+ * @return A boolean indicating if the help argument was given
+ */
+bool checkHelp(int argc, char *argv[]);
+
+/**
+ * Show help page for ODE simulator
+ */
+void showHelp();
+
+/**
  * Run the ode simulator given some command line arguments
  * @param argc The number of command line arguments
  * @param argv The command line arguments
