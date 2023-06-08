c3a42703479ebc96e0dfa42161d9d643a3e8b34b
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-06-08 10:50:49 +0200

    Created input transform class

---
+++ src/pde/parser/transform.cc
@@ -0,0 +1,13 @@
+
+#include "transform.h"
+
+using namespace pde::parser;
+
+InputTransform::InputTransform(PreSystem &preSystem) :
+preSystem(preSystem) { }
+
+InputTransform::~InputTransform() { }
+
+void InputTransform::run() {
+    // TODO
+}
