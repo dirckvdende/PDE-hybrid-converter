39ffe4d341dd0c001e48969c27f9ddd37088cc1e
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2023-06-08 15:19:43 +0200

    Implemented system transformer

--- src/pde/parser/transform.h
+++ src/pde/parser/transform.h
@@ -2,6 +2,8 @@
 #pragma once
 
 #include "presystem.h"
+#include <string>
+#include <unordered_map>
 
 namespace pde::parser {
 
@@ -31,9 +33,50 @@ public:
 
 private:
 
+    /**
+     * Collect information about variable names and generate new equations for
+     * derivatives
+     */
+    void genEquations();
+
+    /**
+     * Generate new boundary conditions and check if boundary conditions are
+     * correct
+     */
+    void genBoundary();
+
+    /**
+     * Check if all initial conditions are present, and transform with new
+     * variables
+     */
+    void genInitial();
+
+    /**
+     * Check if all bounds are present, and transform with new variables
+     */
+    void genBounds();
+
+    /**
+     * General function for initial conditions and bounds, to check if all
+     * derivatives are present, and to replace derivatives with variable names
+     * @param nodes The nodes list that corresponds with either initial
+     * conditions or bounds
+     */
+    void checkAllDerivs(std::vector<expr::ExprNode> &nodes);
+
+    /**
+     * Alternative variable name for nth derivative
+     * @param var The original variable name
+     * @param n The derivative
+     * @return The alternative variable name
+     */
+    static std::string derivName(const std::string &var, size_t n);
+
     // A reference to the presystem to tranform, note that the system will
     // be modified by this object
     PreSystem &preSystem;
+    // List of variables and their derivative counts
+    std::unordered_map<std::string, size_t> vars;
 
 };
 
